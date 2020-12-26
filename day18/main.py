388b5b556a7a6eb30f786ee5366fd39eb5958b80
Reinhard Michalke <reinhardmichalke@gmail.com>
2020-12-26 10:01:34 +0100

    added day18 solution

--- day18/main.py
+++ day18/main.py
@@ -3,6 +3,10 @@ from typing import Set, Tuple
 import networkx as nx
 import itertools
 import functools
+import multiprocessing
+
+pool = multiprocessing.Pool()
+
 
 def add_all_neighbors(graph: nx.Graph, grid, p):
     x, y = p
@@ -19,7 +23,7 @@ def add_all_neighbors(graph: nx.Graph, grid, p):
 def parse_input():
     keys = {}
     doors = {}
-    pos = None
+    pos = []
     graph = nx.Graph()
     with open('input') as f:
         grid = [[c for c in s.strip()] for s in f.readlines()]
@@ -27,7 +31,7 @@ def parse_input():
             for x in range(len(grid[0])):
                 tile = grid[y][x]
                 if tile == '@':
-                    pos = (x, y)
+                    pos.append((x, y))
                 elif tile == '#':
                     continue
                 else:
@@ -38,40 +42,77 @@ def parse_input():
                         doors[(x, y)] = tile
     graph = nx.relabel_nodes(graph, doors)
     graph = nx.relabel_nodes(graph, keys)
-    return graph, pos, set(doors.values()), set(keys.values())
+    return graph, tuple(pos), set(doors.values()), set(keys.values())
 
 
-def can_reach(target, source, obtained_keys, apsp):
-    path = apsp[source][target]
-    for n in path:
-        if isinstance(n, str) and n.isupper():
-            if not n.lower() in obtained_keys:
-                return False
-    return True
+def can_reach(target, source, obtained_keys):
+    global keys_needed_on_path
+    global shortest_paths
+    key_needed = keys_needed_on_path[source][target]
+    if 'unreachable' not in key_needed:
+        return key_needed.issubset(obtained_keys)
+    return False
 
 
-def get_reachable_keys(pos, keys, obtained_keys, apsp):
+def get_reachable_keys(pos, obtained_keys, apsp):
+    global keys
     return {k for k in keys if can_reach(k, pos, obtained_keys, apsp)}.difference(obtained_keys)
 
-def get_distance_to_keys(pos, keys, obtained_keys, apsp):
-    return {k: len(apsp[pos][k]) -1 for k in keys.difference(obtained_keys) if can_reach(k, pos, obtained_keys, apsp)}
 
-def play_game(pos, obtained_keys: Set, keys: Set, apsp):
-    print("Game: {} - {} - {}".format(pos, len(obtained_keys), len(keys.difference(obtained_keys))))
-    if keys.issubset(obtained_keys):
+def get_distance_to_keys(pos, obtained_keys, apsp):
+    return {k: len(apsp[pos][k]) - 1 for k in keys.difference(obtained_keys) if can_reach(k, pos, obtained_keys) and len(apsp[pos][k]) > 0}
+
+
+def get_reachable_for_several_pos(positions, obtained_keys):
+    reachable = {}
+    for i, pos in enumerate(positions):
+        reachable[i] = get_distance_to_keys(pos, obtained_keys, shortest_paths)
+    return reachable
+seen = {}
+
+
+def play_game(positions, obtained_keys):
+    global shortest_paths
+    global keys
+    global doors
+    obtained_str = ''.join(sorted(obtained_keys))
+    if obtained_keys == keys:
+        seen[(positions, obtained_str)] = 0
         return 0
-    reachable = get_distance_to_keys(pos, keys, obtained_keys, apsp)
-    distances = map(lambda x: reachable[x] + play_game(x, obtained_keys.union(set(apsp[pos][x]).intersection(keys)), keys, apsp), reachable.keys())
-    return min(distances)
+    if (positions, obtained_str) in seen:
+        return seen[(positions, obtained_str)]
+    reachable = get_reachable_for_several_pos(positions, obtained_keys)
+    next_args = []
+    for i in reachable.keys():
+        for key in reachable[i].keys():
+            new_pos = [x for x in positions]
+            new_pos[i] = key
+            next_args.append((i, tuple(new_pos), obtained_keys.union(key)))
+    possibilities = []
+    for a in next_args:
+        idx = a[0]
+        new_pos = a[1][a[0]]
+        distance = reachable[idx][new_pos]
+        possibilities.append(distance + play_game(a[1], a[2]))
+    ret = min(possibilities)
 
+    seen[(positions, obtained_str)] = ret
+    return min(possibilities)
 
 
 g, pos, doors, keys = parse_input()
-relevant_nodes = {v for v in doors}.union(v for v in keys).union({pos})
-apsp = dict()
-for t in list(itertools.product(list(relevant_nodes),list(relevant_nodes))):
+relevant_nodes = {v for v in doors}.union(v for v in keys).union(set(pos))
+shortest_paths = dict()
+keys_needed_on_path = dict()
+for t in list(itertools.product(list(relevant_nodes), list(relevant_nodes))):
     u, v = t
-    if u not in apsp:
-        apsp[u] = dict()
-    apsp[u][v] = nx.shortest_path(g, u, v)
-print(play_game(pos, set(), keys, apsp))
+    if u not in shortest_paths:
+        shortest_paths[u] = dict()
+        keys_needed_on_path[u] = dict()
+    try:
+        shortest_paths[u][v] = set(nx.shortest_path(g, u, v))
+        keys_needed_on_path[u][v] = set(map(lambda x: x.lower(), shortest_paths[u][v].intersection(doors)))
+    except nx.NetworkXNoPath:
+        shortest_paths[u][v] = set()
+        keys_needed_on_path[u][v] = set('unreachable')
+print(play_game(pos, set()))
